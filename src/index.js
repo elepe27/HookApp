266025f9e6b89a4acf2b7dcef21bdaefb8a5bdf4
Esteban Eduardo Lepe Castro <esteban.lepe1c@gmail.com>
2021-12-17 13:35:23 -0300

    useState

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CounterApp } from './components/01-useState/CounterApp';
+// import { HooksApp } from './HooksApp';
+
+ReactDOM.render(
+ 
+    <CounterApp/>,
+  
+  document.getElementById('root')
+);
+
+
+
